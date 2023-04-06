0e5e685bdd49099b40fd1cb3c1fc7d740592466e
FtyLollipop <ftylollipop@outlook.com>
2023-04-06 10:09:11 +0800

    Update index.js

--- death.message/index.js
+++ death.message/index.js
@@ -1,4 +1,4 @@
-let config = new JsonConfigFile('plugins/sparkbridge/death.message/config/config.json')
+let config = new JsonConfigFile('plugins/sparkbridge/death.message/config/config.json', {'groups': [], 'edition': 'Java'})
 let entityData = (new JsonConfigFile(`plugins/sparkbridge/death.message/config/entity${config.get('edition')}.json`)).get("entity")
 let messageData = (new JsonConfigFile(`plugins/sparkbridge/death.message/config/message${config.get('edition')}.json`)).get("message")
 let mapData = (new JsonConfigFile('plugins/sparkbridge/death.message/config/map.json')).get("map")
@@ -14,7 +14,7 @@ function onStart(adapter){
 function info(){
     return {
         name : 'death.message',
-        desc : '死亡信息转发到群聊',
+        desc : '死亡消息转发到群聊',
         author : 'FtyLollipop',
         version : [0,0,1]
     }
@@ -27,17 +27,16 @@ function stringFormat(str, args) {
 }
 
 function deathEventHandler(mob, source, cause, entity, message, map) {
-    let args = []
     let msg = ''
+    let args = []
     if(!mob.isPlayer()) { return false }
     msg = message[map.exception?.[source?.type]?.[cause]]
     if(!msg) {
-        msg = message[map[cause]] || `%s死了 %插件消息数据需要更新 source:${args[0]} cause:${cause}%`
+        msg = message?.[map?.[cause]] || `%s${message['death.attack.generic']} %插件消息数据需要更新 source:${args[0]} cause:${cause}%`
     }
     args.push(mob.name)
-    console.log(source)
     if(source) {
-        args.push(entity[source?.type] || source.name)
+        args.push(entity?.[source?.type] || source?.name)
     }
     return stringFormat(msg, args)
 }
