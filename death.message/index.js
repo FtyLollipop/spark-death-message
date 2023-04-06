93c16c9a71b4f314201ca840332ee602423c8b06
FtyLollipop <ftylollipop@outlook.com>
2023-04-06 19:24:47 +0800

    增加条目

--- death.message/index.js
+++ death.message/index.js
@@ -1,13 +1,13 @@
 let config = new JsonConfigFile('plugins/sparkbridge/death.message/config.json', '{"groups": [], "edition": "java"}')
-let entityData = (new JsonConfigFile(`plugins/sparkbridge/death.message/resources/entity.json`)).get(config.get('edition'))
-let messageData = (new JsonConfigFile(`plugins/sparkbridge/death.message/resources/message.json`)).get(config.get('edition'))
-let mapData = (new JsonConfigFile('plugins/sparkbridge/death.message/resources/map.json')).get("map")
+let entityData = (new JsonConfigFile('plugins/nodejs/sparkbridge/plugins/death.message/resources/entity.json')).get(config.get('edition'))
+let messageData = (new JsonConfigFile('plugins/nodejs/sparkbridge/plugins/death.message/resources/message.json')).get(config.get('edition'))
+let mapData = (new JsonConfigFile('plugins/nodejs/sparkbridge/plugins/death.message/resources/map.json')).get("map")
 
 function onStart(adapter){
     mc.listen('onMobDie', (mob, source, cause) => {
         let msg = deathEventHandler(mob, source, cause, entityData, messageData, mapData)
         if(!msg) return
-        config.get("groups").forEach(g => adapter.sendGroupMsg(g, msg))
+        config.get('groups').forEach(g => adapter.sendGroupMsg(g, msg))
     })
 }
 
@@ -32,7 +32,7 @@ function deathEventHandler(mob, source, cause, entity, message, map) {
     if(!mob.isPlayer()) { return null }
     msg = message?.[map.exception?.[source?.type]?.[cause]] ?? null
     if(!msg) {
-        msg = message?.[map?.[cause]] ?? `%s${message['death.attack.generic']} %插件消息数据需要更新 source:${args[0]} cause:${cause}%`
+        msg = message?.[map?.[cause]] ?? `${message['death.attack.generic']} %插件消息数据需要更新 source:${args[0]} cause:${cause}%`
     }
     args.push(mob.name)
     if(source) {
