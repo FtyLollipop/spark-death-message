931470fecd4e5abbfea5424990decd8a3a19f407
FtyLollipop <ftylollipop@outlook.com>
2023-04-07 20:01:49 +0800

    Update index.js

--- death.message/index.js
+++ death.message/index.js
@@ -1,14 +1,14 @@
-let config = new JsonConfigFile('plugins/sparkbridge/death.message/config.json', '{"groups": [], "edition": "java"}')
-let entityData = (new JsonConfigFile('plugins/nodejs/sparkbridge/plugins/death.message/resources/entity.json')).get(config.get('edition'))
-let messageData = (new JsonConfigFile('plugins/nodejs/sparkbridge/plugins/death.message/resources/message.json')).get(config.get('edition'))
-let mapData = (new JsonConfigFile('plugins/nodejs/sparkbridge/plugins/death.message/resources/map.json')).get("map")
+const config = new JsonConfigFile('plugins/sparkbridge/death.message/config.json', '{"groups": [], "edition": "java"}')
+const entityData = (new JsonConfigFile('plugins/nodejs/sparkbridge/plugins/death.message/resources/entity.json')).get(config.get('edition'))
+const messageData = (new JsonConfigFile('plugins/nodejs/sparkbridge/plugins/death.message/resources/message.json')).get(config.get('edition'))
+const mapData = (new JsonConfigFile('plugins/nodejs/sparkbridge/plugins/death.message/resources/map.json')).get("map")
 
 function onStart(adapter){
-    let groups = config.get('groups')
+    const groups = config.get('groups')
     logger.setConsole(config.get('islogprt'))
     logger.setFile(config.get('islogfile') ? 'logs/death.message.log' : null)
     mc.listen('onMobDie', (mob, source, cause) => {
-        let msg = deathEventHandler(mob, source, cause, entityData, messageData, mapData)
+        const msg = deathEventHandler(mob, source, cause, entityData, messageData, mapData)
         if(!msg) return
         logger.info(msg)
         groups.forEach(g => adapter.sendGroupMsg(g, msg))
@@ -25,8 +25,8 @@ function info(){
 }
 
 function stringFormat(str, args) {
-    let regex = /%s/
-    let _r=(p,c) => p.replace(regex,c)
+    const regex = /%s/
+    const _r=(p,c) => p.replace(regex,c)
     return args.reduce(_r, str)
 }
 
@@ -34,7 +34,25 @@ function deathEventHandler(mob, source, cause, entity, message, map) {
     let msg = null
     let args = []
     if(!mob.isPlayer()) { return null }
-    msg = message?.[map.exception?.[source?.type]?.[cause]] ?? null
+    const pos = mc.getPlayer(mob.name).lastDeathPos
+    if(cause === 1) {
+        for(let x = -1; x <= 1; x++) {
+            for(let y = -2; y <= 1; y++) {
+                for(let z = -1; z <= 1; z++) {
+                    const block = mc.getBlock(pos.x + x, pos.y + y, pos.z + z, pos.dimid)?.type
+                    if(block === 'minecraft:cactus') {
+                        msg = message['death.attack.cactus']
+                        break
+                    } else if (block === 'minecraft:sweet_berry_bush') {
+                        msg = message['death.attack.sweetBerry']
+                        break
+                    }
+                }
+            }
+        }
+    } else {
+        msg = message?.[map.exception?.[source?.type]?.[cause]] ?? null
+    }
     if(!msg) {
         msg = message?.[map?.[cause]] ?? `${message['death.attack.generic']} %插件消息数据需要更新 source:${args[0]} cause:${cause}%`
     }
